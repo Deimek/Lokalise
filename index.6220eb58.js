b6798cc33ea592e0dfb86efd2b8598e4d0995d34
Deimek <333olegovich333@gmail.com>
2024-03-26 18:31:17 +0100

    Updates

---
+++ index.6220eb58.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelector(".burger"),s=document.querySelector(".header-nav");(e||s)&&e.addEventListener("click",()=>{s.classList.contains("show")?s.classList.remove("show"):s.classList.add("show")})})();
+//# sourceMappingURL=index.6220eb58.js.map
